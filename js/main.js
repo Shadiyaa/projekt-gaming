6ac1f0d15336760d756a59359cc52f247953b436
Iwona Kowalczyk <fionka7@gmail.com>
2020-04-17 09:52:45 +0200

    added team section

--- js/main.js
+++ js/main.js
@@ -10,14 +10,41 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     window.addEventListener('scroll', addShadow);
 
-    $(document).ready(function () {
-        $(document).click(function (event) {
-            var clickover = $(event.target);
-            var _opened = $(".navbar-collapse").hasClass("show");
-            if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-                $(".navbar-toggler").click();
-            }
-        });
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
     });
-    
+
+    $('.team-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        mobileFirst: true,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 4
+                }
+            }
+        ]
+    });
+
 });
